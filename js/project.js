ac8ad2aa2d0e41c86117e1bf16b7983f39af101e
Michael Harris <mike.harris@cerner.com>
2015-10-05 18:15:36 -0500

    Loaded data, basic functions
    Added all the provided data
    Auto complete search box added, needs css
    Grid and list view buttons added, with toggle states
    Initial grid view added.

---
+++ js/project.js
@@ -0,0 +1,14 @@
+RenderGrid = function(movies){
+	var $movieContainer = $(".movie-container").empty()
+	var current = {}
+	for(var i = 0, len = movies.length; i < len; i++){
+		current = movies[i];
+		$movieContainer.append(
+			"<div class='movie'>"+"<img src='" + current.photo + "'>" + "</div>"
+		);
+	}
+};
+
+RenderList = function(movies){
+	var $movieContainer = $(".movie-container").empty()
+}
